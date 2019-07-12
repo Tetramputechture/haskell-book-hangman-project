9d219b08deeb56781a7974282320e67806a61257
Nick Anderson <tetramputechture@gmail.com>
2019-07-12 12:51:10 -0500

    Add allWords function to Main

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,12 @@
 module Main where
 
+type WordList = [String]
+
+allWords :: IO WordList
+allWords = do
+  dict <- readFile "data/dict.txt"
+  return (lines dict)
+
 main :: IO ()
 main = do
   putStrLn "hello world"
