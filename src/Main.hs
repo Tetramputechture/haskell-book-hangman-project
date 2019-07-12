aa30463bf55a5b61f768e163cd0b914bc0f25336
Nick Anderson <tetramputechture@gmail.com>
2019-07-12 12:55:27 -0500

    Add gameWords function along with min and max word lengths

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,21 @@ allWords = do
   dict <- readFile "data/dict.txt"
   return (lines dict)
 
+minWordLength :: Int
+minWordLength = 5
+
+maxWordLength :: Int
+maxWordLength = 9
+
+gameWords :: IO WordList
+gameWords = do
+  aw <- allWords
+  return (filter gameLength aw)
+  where gameLength w = 
+          let l = length (w :: String)
+          in      l >= minWordLength
+              &&  l <  maxWordLength
+
 main :: IO ()
 main = do
   putStrLn "hello world"
