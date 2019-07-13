8cc1e280bd716e5d585c23f445daf5da5bd7a4bf
Nick Anderson <tetramputechture@gmail.com>
2019-07-13 16:55:16 -0500

    Add methods for running the game

--- src/Main.hs
+++ src/Main.hs
@@ -44,6 +44,56 @@ fillInCharacter (Puzzle word
         zipWith (zipper c)
           word filledInSofar
 
+handleGuess :: Puzzle -> Char -> IO Puzzle
+handleGuess puzzle guess = do
+  putStrLn $ "Your guess was: " ++ [guess]
+  case (charInWord puzzle guess
+      , alreadyGuessed puzzle guess) of
+    (_, True) -> do
+      putStrLn "You already guessed that\
+              \ character, pick \
+              \ something else!"
+      return puzzle
+    (True, _) -> do
+      putStrLn "This character was in the\
+              \ word, filling in the word\
+              \ accordingly"
+      return (fillInCharacter puzzle guess)
+    (False, _) -> do
+      putStrLn "This character wasn't in\
+              \ the word, try again."
+      return (fillInCharacter puzzle guess)
+
+gameOver :: Puzzle -> IO ()
+gameOver (Puzzle wordToGuess _ guessed) =
+  if (length guessed) > 7 then
+    do putStrLn "You lose!"
+       putStrLn $
+         "The word was: " ++ wordToGuess
+       exitSuccess
+  else return()
+
+gameWin :: Puzzle -> IO ()
+gameWin (Puzzle _ filledInSoFar _) =
+  if all isJust filledInSoFar then
+    do putStrLn "You win!"
+      exitSuccess
+  else return ()
+
+runGame :: Puzzle -> IO ()
+runGame puzzle = forever $ do
+  gameOver puzzle
+  gameWin puzzle
+  putStrLn $
+    "Current puzzle is: " ++ show puzzle
+  putStr "Guess a letter: "
+  guess <- getLine
+  case guess of
+    [c] -> handleGuess puzzle c >>= runGame
+    _   ->
+      putStrLn "Your guess must\
+              \ be a single character"
+
 allWords :: IO WordList
 allWords = do
   dict <- readFile "data/dict.txt"
@@ -74,4 +124,4 @@ randomWord' = gameWords >>= randomWord
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn "hello worlddd"
